3f4e6f0a77095b8b42ffce99b629264b7490e00f
theflag505 <hamdan.mobeen@gmail.com>
2023-03-19 06:30:18 -0700

    implement feature add student

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -5,23 +5,61 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
-import {DataGrid} from '@mui/x-data-grid';
-import {SEMESTER_LIST} from '../constants.js'
+import { DataGrid } from '@mui/x-data-grid';
+import { SEMESTER_LIST } from '../constants.js'
+import { SERVER_URL } from '../constants.js'
+import Cookies from 'js-cookie';
+import { ToastContainer, toast } from 'react-toastify';
+import AddStudent from './AddStudent';
+import { ButtonGroup } from '@mui/material';
 
 // user selects from a list of  (year, semester) values
 class Semester extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {selected: SEMESTER_LIST.length-1 };
-    }
- 
-   onRadioClick = (event) => {
-    console.log("Semester.onRadioClick "+JSON.stringify(event.target.value));
-    this.setState({selected: event.target.value});
+  constructor(props) {
+    super(props);
+    this.state = { selected: SEMESTER_LIST.length - 1 };
   }
-  
-  render() {    
-      const icolumns = [
+
+  onRadioClick = (event) => {
+    console.log("Semester.onRadioClick " + JSON.stringify(event.target.value));
+    this.setState({ selected: event.target.value });
+  }
+
+  addStudent = (student) => {
+    console.log("In function call")
+    const token = Cookies.get('XSRF-TOKEN');
+
+    fetch(`${SERVER_URL}/student`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-XSRF-TOKEN': token
+        },
+        body: JSON.stringify(student)
+      })
+      .then(res => {
+        if (res.ok) {
+          toast.success("Student successfully added", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+        } else {
+          toast.error("Error when adding", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error('Post http status =' + res.status);
+        }
+      })
+      .catch(err => {
+        toast.error("Error when adding", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err);
+      })
+  }
+
+  render() {
+    const icolumns = [
       {
         field: 'id',
         headerName: 'Year',
@@ -35,34 +73,39 @@ class Semester extends Component {
               color="default"
               size="small"
             />
-            { SEMESTER_LIST[params.row.id].year }
+            {SEMESTER_LIST[params.row.id].year}
           </div>
         )
       },
       { field: 'name', headerName: 'Semester', width: 200 }
-      ];       
-       
+    ];
+
     return (
-       <div>
-         <AppBar position="static" color="default">
-            <Toolbar>
-               <Typography variant="h6" color="inherit">
-                  Schedule - select a term
-               </Typography>
-            </Toolbar>
-         </AppBar>
-         <div align="left" >
-              <div style={{ height: 400, width: '100%', align:"left"   }}>
-                <DataGrid   rows={SEMESTER_LIST} columns={icolumns} />
-              </div>                
-              <Button component={Link} 
-                      to={{pathname:'/schedule' , 
-                      year:SEMESTER_LIST[this.state.selected].year, 
-                      semester:SEMESTER_LIST[this.state.selected].name}} 
-                variant="outlined" color="primary" style={{margin: 10}}>
-                Get Schedule
-              </Button>
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Schedule - select a term
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div align="left" >
+          <div style={{ height: 400, width: '100%', align: "left" }}>
+            <DataGrid rows={SEMESTER_LIST} columns={icolumns} />
           </div>
+          <ButtonGroup>
+            <AddStudent addStudent={this.addStudent} />
+          </ButtonGroup>
+          <Button component={Link}
+            to={{
+              pathname: '/schedule',
+              year: SEMESTER_LIST[this.state.selected].year,
+              semester: SEMESTER_LIST[this.state.selected].name
+            }}
+            variant="outlined" color="primary" style={{ margin: 10 }}>
+            Get Schedule
+          </Button>
+        </div>
       </div>
     )
   }
